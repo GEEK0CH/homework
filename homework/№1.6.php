cba7e6a855a3280d8888878af5edfbbc387954f4
GEEK0CH <natanbayrakov02@mail.ru>
2022-08-21 00:32:35 +0300

    Work 1.6

---
+++ homework/№1.6.php
@@ -0,0 +1,21 @@
+<?php
+echo '<table>';
+for ($i=1; $i<=10; $i++) {
+    echo '<tr>';
+    echo '<td>';
+    echo "[$i]";
+    echo '</td>';
+
+    for ($b=2; $b<=10; $b++) {
+        echo '<td>';
+        if ($i == 1) {
+            echo "[" . $i*$b . "]";
+        } elseif ($b % 2 == 0) {
+            echo "(" . $i*$b . ")";
+        } else {
+            echo "[" . $i*$b . "]";
+        }
+        echo '</td>';
+    }
+    echo '</tr>';
+}
