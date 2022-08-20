af8b9a3e9aa951eb639cf069db6588e6b029b42d
GEEK0CH <natanbayrakov02@mail.ru>
2022-08-20 23:36:32 +0300

    Work 1.5

---
+++ homework/№1.5.php
@@ -0,0 +1,31 @@
+<?php
+
+$bmw = [
+        'model' => 'X5',
+        'speed' => '120',
+        'doors' => '5',
+        'year' => '2015'
+];
+$toyota = [
+        'model' => 'camry',
+        'speed' => '150',
+        'doors' => '5',
+        'year' => '2020'
+];
+$opel = [
+        'model' => 'astra',
+        'speed' => '80',
+        'doors' => '3',
+        'year' => '2016'
+];
+
+$cars = [
+    'bmw' => $bmw ,
+    'toyota' => $toyota ,
+    'opel' => $opel
+];
+
+foreach ($cars as $name => $array) {
+    echo "Car" ." $name<br>";
+    echo implode(' ', $array)."<br>";
+}
